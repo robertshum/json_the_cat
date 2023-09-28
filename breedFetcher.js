6a00285ffa4f2015061fd35254aab5dd6c0c36f1
Robert Shum <robertshum@gmail.com>
2023-09-27 17:06:52 -0700

    added tests and mocha/chai framework for testing.

--- breedFetcher.js
+++ breedFetcher.js
@@ -1,34 +1,30 @@
 const request = require("request");
 
-
-const arg = process.argv.slice(2, 3);
-const url = `https://api.thecatapi.com/v1/breeds/search?q=${arg}`;
-
-const makeRequest = (url) => {
-  console.log("url:", url);
-  request(url, (error, response, body) => {
+const fetchBreedDescription = (breedName, callback) => {
+  const url = `https://api.thecatapi.com/v1/breeds/search?q=${breedName}`;
+  request(url, (error, _response, body) => {
 
     // Print the response status code if a response was received
-    console.log("statusCode:", response && response.statusCode);
+    //console.log("statusCode:", response && response.statusCode);
     if (error) {
-      console.log(`Error loading website ${url}`, error);
+
+      //callback expects error first, then data
+      callback(error, null);
+      //console.log(`Error loading website ${url}`, error);
       return;
     }
-
-    //all gucci
-    console.log(typeof body);
 
     //Instruction  Let's use JSON.parse to convert the JSON string into an actual object.
     const data = JSON.parse(body);
 
     //if it's empty
     if (data.length === 0) {
-      console.log("No cats found.  Meow.");
+      callback("No cats found", null);
       return;
     }
 
-    console.log("first entry:", data[0].description);
+    callback(null, data[0].description);
   });
 };
 
-makeRequest(url);
+module.exports = { fetchBreedDescription };
